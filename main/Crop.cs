84ed4f44feedf0f50d47336931eed468d7d5df21
Gui Costa <guibscosta@hotmail.com>
2024-01-10 03:08:10 +0000

    Crop pricing done

--- main/Crop.cs
+++ main/Crop.cs
@@ -3,7 +3,8 @@ using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
 using System;
 using System.Linq;
-using SObject  = StardewValley.Object;
+using System.Xml.Linq;
+using SObject = StardewValley.Object;
 #nullable enable
 
 namespace ProfitCalculator.main
@@ -11,23 +12,26 @@ namespace ProfitCalculator.main
     // retrives and stores crop data from game files
     internal class Crop
     {
-        private int Id;
-        private Item Item;
-        private string Name;
-        private Tuple<Texture2D, Rectangle>? Sprite;
-        private bool IsTrellisCrop;
-        private bool IsGiantCrop;
-        private Tuple<Texture2D, Rectangle>? GiantSprite;
-        private Item[]? Seeds;
-        private int[] Phases;
-        private Texture2D?[]? PhaseSprites;
-        private int Regrow;
-        private bool IsPaddyCrop;
+        public int Id;
+        public Item Item;
+        public string Name;
+        public Tuple<Texture2D, Rectangle>? Sprite;
+        public bool IsTrellisCrop;
+        public bool IsGiantCrop;
+        public Tuple<Texture2D, Rectangle>? GiantSprite;
+        public Item[]? Seeds;
+        public int[] Phases;
+        public Texture2D?[]? PhaseSprites;
+        public int Regrow;
+        public bool IsPaddyCrop;
 
-        private WorldDate? StartDate;
-        private WorldDate? EndDate;
-        private int Days;
-        private int Price;
+        public WorldDate? StartDate;
+        public WorldDate? EndDate;
+        public int Days;
+        public int Price = -1;
+        //prices should be calculated from price, then price x1.25, then price x1.5, then price x2
+        public int[] Prices = new int[4];
+
 
         public Crop(int id, Item item, string name, Tuple<Texture2D, Rectangle>? sprite, bool isTrellisCrop, bool isGiantCrop, Tuple<Texture2D, Rectangle>? giantSprite, Item[]? seeds, int[] phases, Texture2D?[]? phaseSprites, int regrow, bool isPaddyCrop, WorldDate? startDate, WorldDate? endDate)
         {
@@ -46,7 +50,17 @@ namespace ProfitCalculator.main
             StartDate = startDate;
             EndDate = endDate;
             Days = Phases.Sum();
-            Price = ((SObject)Item).salePrice();
+            Price = ((SObject)Item).Price;
+            Prices[0] = Price;
+            Prices[1] = (int)(Price * 1.25);
+            Prices[2] = (int)(Price * 1.5);
+            Prices[3] = Price * 2;
+            for (int i = 0; i < Prices.Length; i++)
+            {
+                Prices[i] = (int)getPriceAfterMultipliers(Prices[i]);
+            }
+
+
         }
 
         public override bool Equals(object? obj)
@@ -74,9 +88,24 @@ namespace ProfitCalculator.main
                 + $"\tStartDate: {StartDate}"
                 + $"\n\tEndDate: {EndDate} "
                 + $"\tDays: {Days} "
-                + $"\n\tPrice: {Price} {Item.Name} {Item.DisplayName}";
+                + $"\n\tPrice: {Price}";
         }
 
+
+        private float getPriceAfterMultipliers(float startPrice)
+        {
+            float modifier = 1f;
+
+            Farmer allFarmer = Game1.player;
+
+            //if tiller then price x1.1
+            if (allFarmer.professions.Contains(Farmer.tiller))
+            {
+                modifier *= 1.1f;
+            }
+
+            return startPrice * modifier;
+        }
         ///to string method prints out all the data for the crop
         ///
     }
