3476d8fafb3f0dc01124d919e3d0533eaf6a9b24
Gui Costa <guibscosta@hotmail.com>
2024-01-20 23:03:14 +0000

    Code cleanup

--- ModAPI.cs
+++ ModAPI.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using ProfitCalculator.main;
 using StardewValley;
+using System;
 using static ProfitCalculator.Utils;
 using Crop = ProfitCalculator.main.Crop;
 
